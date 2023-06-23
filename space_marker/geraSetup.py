3ce2b503308e3fe6718973db01c79f4caba01e84
LuisFP0205 <luisfelipepagnussat55@gmail.com>
2023-06-23 20:32:53 -0300

    comit 8
    arrumado erro no geraSetup pois estava gerando um .exe que não permanecia aberto

--- space_marker/geraSetup.py
+++ space_marker/geraSetup.py
@@ -1,16 +1,16 @@
 #pip install cx_freeze
 import cx_Freeze
 executables =[
-    cx_Freeze.Executable(script="space_marker.py", icon="assets/space.ico")
+    cx_Freeze.Executable(script="space_marker.py", icon="space.ico")
 ]
 cx_Freeze.setup(    
     name = "Space Machine Power",
     options={
         "build_exe":{
             "packages":["pygame","os","time","random","pickle"],
-            "include_files":["assets/bg.jpg",
-                            "assets/space.png",
-                            "assets/Space_Machine_Power.mp3"]
+            "include_files":["bg.jpg",
+                            "space.png",
+                            "Space_Machine_Power.mp3"]
         }
     }, executables = executables
 )
